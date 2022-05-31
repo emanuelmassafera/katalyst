38ae278c0e6d9ad064d2b2952b300eee63b9f85b
Emanuel Massafera <emanuel301@live.com>
2022-05-31 09:20:05 -0300

    feat: add FizzBuzz problem

---
+++ fizzbuzz.js
@@ -0,0 +1,8 @@
+module.exports = class FizzBuzz {
+  convert(number) {
+    if (number % 15 == 0) return 'FizzBuzz';
+    if (number % 3 == 0) return 'Fizz';
+    if (number % 5 == 0) return 'Buzz';
+    return String(number);
+  }
+} 
